f9df29d3d8f41131ade586c5ce61349e716f9a8f
ItsVaishnav <vaishnavghadgepc99@gmail.com>
2025-08-04 11:17:03 +0530

    Toto App final Version

--- to-do-app-v1/src/App.jsx
+++ to-do-app-v1/src/App.jsx
@@ -2,19 +2,57 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import ShowTodo from "./components/ShowTodo";
 import Container from "./components/Container";
+import { useState } from "react";
+import Errormessage from "./components/Errormessage";
 function App() {
-  const items = [
+
+  const [items ,setItems] = useState([
     {todoname : "Buy Milk",tododate : "20/07/2025"},
     {todoname : "Buy Eggs",tododate : "28/07/2025"},
-  ]
-  return (
+  ]);
+
+  const [name , setName] = useState('');
+  const [date , setDate] = useState('');
+
+  const [error , setError] = useState('');
+  const [errorDesc , setErrorDesc] = useState('');
+
+  const HandleOnAdd = () => {
+    console.log("Hello");
+    if(name === "" || date === ''){
+      setError("Empty Items");
+      setErrorDesc("Please Enter Somthing to add ....!");
+    }else if (items.some((item) => item.todoname === name && item.tododate === date)){
+      setError("Dulicate Item");
+      setErrorDesc("This todo has been already added ...!");
+    }else{
+      let newItems = [...items , {todoname : name,tododate : date}];
+      setItems(newItems);
+      setError("ToDo Added");
+      setErrorDesc("Your todo has been added successfully...!");
+    }
+    setName('');
+    setDate('');
+  }
+
+  const HandleOnChangeName = (event) => {
+    console.log(event);
+    setName(event.target.value);
+  }
+  const HandleOnChangeDate = (event) => {
+    console.log(event);
+    setDate(event.target.value);
+  }
+
+  return (  
     <>
       <div className="to-do-container text-center conn">
         <Container>
         <AppName />
+        <Errormessage error={error} errorDesc={errorDesc}/>
         </Container>
         <Container>
-        <AddTodo />
+        <AddTodo HandleOnAdd={HandleOnAdd} HandleOnChangeName={HandleOnChangeName} HandleOnChangeDate={HandleOnChangeDate} name={name} date={date}/>
         <div className="container text-center">
           {items.map(ele=> (
             <ShowTodo key={ele.todoname} element={ele}/>
--- to-do-app-v1/src/components/AddTodo.jsx
+++ to-do-app-v1/src/components/AddTodo.jsx
@@ -1,20 +1,18 @@
 import styles from "./AddTodo.module.css";
-import classNames from 'classnames';
-function AddTodo() {
+
+function AddTodo({HandleOnAdd, HandleOnChangeDate, HandleOnChangeName , name , date}) {
   return (
-    // <div className={styles.conn}>
     <div className={`${styles.conn} ${styles.color}`}>
-    {/* <div className={classNames(styles.color, styles.conn)}> */}
     <div className="container text-center">
       <div className="row my-4">
         <div className="col-6">
-          <input type="text" name="" id="" placeholder="Enter To Do Here" />
+          <input type="text" name="" id="" placeholder="Enter To Do Here" onChange={HandleOnChangeName} value={name}/>
         </div>
         <div className="col-4">
-          <input type="date" name="" id="" />
+          <input type="date" name="" id="" onChange={HandleOnChangeDate} value={date}/>
         </div>
         <div className="col-2">
-          <button type="button" className="btn btn-success">
+          <button type="button" className="btn btn-success" onClick={HandleOnAdd}>
             Add
           </button>
         </div>
